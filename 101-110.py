0cb6b76021da5634b6a0fd9232f74cf3cd7f37c0
Oh HyeongMin <100303513+kkomabear1227@users.noreply.github.com>
2023-01-19 15:41:11 +0900

    Create 101-110.py

---
+++ 101-110.py
@@ -0,0 +1,60 @@
+## 101
+# what is data type with True or False?
+""" Ans : bool """
+
+## 102
+# predict result
+print(3 == 5) # False
+
+## 103
+# predict result
+print(3 < 5) # True
+
+## 104
+# predict result
+x = 4
+print(1 < x < 5) # True! (not Syntax error)
+
+## 105
+# predict result
+print((3 == 3) and (4 != 3)) # True
+
+## 106
+# describe why error occurs
+"""print(3 => 4) # invalid operator 
+"""
+
+## 107
+# predict result
+if 4 < 3: # False
+  print("Hello World") # Not executed
+
+## 108
+# predict result
+if 4 < 3: # False
+  print("Hello World") # Not executed
+else:
+  print("Hi, there.") # executed
+
+## 109
+# predict result
+if True :
+    print ("1")
+    print ("2")
+else :
+    print("3")
+print("4")
+# 1\n2\n4\n
+
+## 110
+# predict result
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
+# 3\n5\n
